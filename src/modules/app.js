b306c142d0ea775f9aef446170aa46e861d7cd4e
mengiefen <mengiefen@gmail.com>
2022-02-28 00:18:52 +0300

    add improvement for navbar

--- src/modules/app.js
+++ src/modules/app.js
@@ -4,12 +4,14 @@ const navbarTogggler = document.querySelector('.navbar-toggler');
 const navbarClose = document.querySelector('.navbar-close');
 const navbar = document.querySelector('.navbar-collapse');
 const logo = document.querySelector('.logo');
+const overlay = document.querySelector('.overlay');
 
 navbarTogggler.addEventListener('click', () => {
   navbar.classList.toggle('open');
   navbarClose.style.display = 'block';
   navbarTogggler.style.display = 'none';
   logo.style.display = 'none';
+  overlay.style.display = 'block';
 });
 
 navbarClose.addEventListener('click', () => {
@@ -17,6 +19,7 @@ navbarClose.addEventListener('click', () => {
   navbarClose.style.display = 'none';
   navbarTogggler.style.display = 'block';
   logo.style.display = 'block';
+  overlay.style.display = 'none';
 });
 
 window.addEventListener('resize', () => {
@@ -28,5 +31,15 @@ window.addEventListener('resize', () => {
   if (window.screen.width < 768) {
     navbarTogggler.style.display = 'block';
     logo.style.display = 'block';
+  }
+});
+
+document.addEventListener('scroll', () => {
+  if (navbar.classList.contains('open')) {
+    navbar.classList.toggle('open');
+    navbarClose.style.display = 'none';
+    navbarTogggler.style.display = 'block';
+    logo.style.display = 'block';
+    overlay.style.display = 'none';
   }
 });
